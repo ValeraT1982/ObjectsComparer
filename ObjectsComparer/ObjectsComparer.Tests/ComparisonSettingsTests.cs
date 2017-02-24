a6f3fe4f3228b435a56e9329860f14ff0e5ee776
valerat1982 <tereschenkoVV@gmail.com>
2017-02-24 14:30:32 -0800

    Custom settings

---
+++ ObjectsComparer/ObjectsComparer.Tests/ComparisonSettingsTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ObjectsComparer.Tests
+{
+    [TestFixture]
+    public class ComparisonSettingsTests
+    {
+        [Test]
+        public void SetgetCustomSettingWithoutKeyTest()
+        {
+            var settings = new ComparisonSettings();
+            settings.SetCustomSetting<string>("test string");
+
+            var settingValue = settings.GetCustomSetting<string>();
+
+            Assert.AreEqual("test string", settingValue);
+        }
+
+        [Test]
+        public void SetgetCustomSettingWithKeyTest()
+        {
+            var settings = new ComparisonSettings();
+            settings.SetCustomSetting<int>(123, "setting1");
+            settings.SetCustomSetting<int>(234, "setting2");
+
+            var setting1Value = settings.GetCustomSetting<int>("setting1");
+            var setting2Value = settings.GetCustomSetting<int>("setting2");
+
+            Assert.AreEqual(123, setting1Value);
+            Assert.AreEqual(234, setting2Value);
+        }
+
+        [Test]
+        public void WronkKeyTest()
+        {
+            var settings = new ComparisonSettings();
+            settings.SetCustomSetting<int>(123, "setting1");
+
+            Assert.Throws<KeyNotFoundException>(() => settings.GetCustomSetting<int>("wrongSettingKey"));
+        }
+
+        [Test]
+        public void WronkTypeTest()
+        {
+            var settings = new ComparisonSettings();
+            settings.SetCustomSetting<int>(123, "setting1");
+
+            Assert.Throws<KeyNotFoundException>(() => settings.GetCustomSetting<double>("setting1"));
+        }
+    }
+}
--- ObjectsComparer/ObjectsComparer/ComparisonSettings.cs
+++ ObjectsComparer/ObjectsComparer/ComparisonSettings.cs
@@ -1,4 +1,7 @@
-﻿namespace ObjectsComparer
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectsComparer
 {
     public class ComparisonSettings
     {
@@ -6,10 +9,31 @@
 
         public bool EmptyAndNullEnumerablesEqual { get; set; }
 
+        private readonly Dictionary<Tuple<Type, string>, object> _settings = new Dictionary<Tuple<Type, string>, object>();
+
         public ComparisonSettings()
         {
             RecursiveComparison = true;
             EmptyAndNullEnumerablesEqual = false;
+        }
+
+        public void SetCustomSetting<T>(T value, string key = null)
+        {
+            var dictionaryKey = new Tuple<Type, string>(typeof(T), key);
+            _settings[dictionaryKey] = value;
+        }
+
+        public T GetCustomSetting<T>(string key = null)
+        {
+            var dictionaryKey = new Tuple<Type, string>(typeof(T), key);
+
+            object value;
+            if (_settings.TryGetValue(dictionaryKey, out value))
+            {
+                return (T) value;
+            }
+
+            throw new KeyNotFoundException();
         }
     }
 }
