4b0b923edca04b5013e2a3153f96c6ca63dc1f54
Valerii Tereshchenko <tereschenkoVV@gmail.com>
2018-07-25 17:21:00 -0700

    migration to vs2018, netstandard2.0 support

--- ObjectsComparer/ObjectsComparer/Properties/AssemblyInfo.cs
+++ ObjectsComparer/ObjectsComparer/Properties/AssemblyInfo.cs
@@ -1,20 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ObjectsComparer")]
-[assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1cefc9b1-3d84-4722-a2a0-0cf89ea9f09a")]
 [assembly: InternalsVisibleTo("ObjectsComparer.Tests")]
