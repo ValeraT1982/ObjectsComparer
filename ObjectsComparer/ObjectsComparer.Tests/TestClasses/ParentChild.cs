cf7e76066766ad004fd530be89dcca5ce8c1c1fd
Ankur Gupta <ankur.gupta@highlands.in>
2022-03-25 14:13:20 +0530

    Tests Added
    I have added the unit tests and ran all the unit tests. All the tests are running fine at my end.

--- ObjectsComparer/ObjectsComparer.Tests/TestClasses/ParentChild.cs
+++ ObjectsComparer/ObjectsComparer.Tests/TestClasses/ParentChild.cs
@@ -1,5 +1,9 @@
 ﻿namespace ObjectsComparer.Tests.TestClasses
 {
+  using System.Collections.ObjectModel;
+
+  using NUnit.Framework;
+
   using ObjectsComparer.Attributes;
 
   public class ParentChild
@@ -13,6 +17,17 @@
       this.Property1 = property1;
     }
 
+
+
+    public ParentChild(Parent myParent,
+                       string property1,
+                       ObservableCollection <Child> children)
+    {
+      this.MyParent = myParent;
+      this.Property1 = property1;
+      this.Children = children;
+    }
+
     #endregion
 
     #region Properties
@@ -21,6 +36,8 @@
 
     [IgnoreInComparison]
     public Parent MyParent { get; set; }
+
+    public ObservableCollection <Child> Children { get; set; } = new ObservableCollection <Child>();
 
     #endregion
   }
