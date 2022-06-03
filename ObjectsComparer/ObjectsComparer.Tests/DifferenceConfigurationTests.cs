0f2bb3598e3d00129c5de535bf39060ff98ff7f3
Daniel Nemec <nemecpub@centrum.cz>
2022-06-03 10:12:49 +0200

    Add DifferenceConfigurationTests class.

---
+++ ObjectsComparer/ObjectsComparer.Tests/DifferenceConfigurationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using NSubstitute;
+using NUnit.Framework;
+using ObjectsComparer.Tests.TestClasses;
+
+namespace ObjectsComparer.Tests
+
+{
+    [TestFixture]
+    public class DifferenceConfigurationTests
+    {
+        [Test]
+        public void PropertyEquality()
+        {
+            var a1 = new A { IntProperty = 10, DateTimeProperty = new DateTime(2017, 1, 1), Property3 = 5 };
+            var a2 = new A { IntProperty = 10, DateTimeProperty = new DateTime(2017, 1, 1), Property3 = 8 };
+            var comparer = new Comparer<A>();
+
+            var isEqual = comparer.Compare(a1, a2);
+
+            Assert.IsTrue(isEqual);
+        }
+    }
+}
