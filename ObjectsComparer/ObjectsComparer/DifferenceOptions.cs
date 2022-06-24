ac47b0529529e2b5b2e404d4a364fb76d07cc86e
nemec <nemec@git-test-bash.com>
2022-06-24 17:48:47 +0200

    Edit DifferenceOptions.

--- ObjectsComparer/ObjectsComparer/DifferenceOptions.cs
+++ ObjectsComparer/ObjectsComparer/DifferenceOptions.cs
@@ -18,32 +18,33 @@ namespace ObjectsComparer
         /// </summary>
         internal static DifferenceOptions Default() => new DifferenceOptions();
 
-        /// <summary>
-        /// Whether the <see cref="Difference"/> instance should contain raw values.
-        /// </summary>
-        public DifferenceOptions IncludeRawValues(bool value)
-        {
-            //DifferenceFactory = value ? d => d : DefaultDifferenceFactory;
+        ///// <summary>
+        ///// Whether the <see cref="Difference"/> instance should contain raw values.
+        ///// </summary>
+        //public DifferenceOptions IncludeRawValues(bool value)
+        //{
+        //    //DifferenceFactory = value ? d => d : DefaultDifferenceFactory;
 
-            return this;
-        }
+        //    return this;
+        //}
 
         /// <summary>
         /// Factory for <see cref="Difference"/> instances.
         /// </summary>
-        internal Func<CreateDifferenceArgs, Difference> DifferenceFactory { get; private set; } = DefaultDifferenceFactory;
+        internal Func<CreateDifferenceArgs, Difference> DifferenceFactory { get; private set; } = null;
 
-        /// <summary>
-        /// Default difference factory.
-        /// If the source difference contains raw values it creates a new <see cref="Difference"/> instance based on the source difference without those values. Otherwise, it returns the source difference itself.
-        /// </summary>
-        public static Func<CreateDifferenceArgs, Difference> DefaultDifferenceFactory => args => args.DefaultDifference;
+        ///// <summary>
+        ///// Default difference factory.
+        ///// If the source difference contains raw values it creates a new <see cref="Difference"/> instance based on the source difference without those values. Otherwise, it returns the source difference itself.
+        ///// </summary>
+        //public static Func<CreateDifferenceArgs, Difference> DefaultDifferenceFactory => args => args.DefaultDifference;
 
         /// <summary>
         /// Factory for <see cref="Difference"/> instances.
         /// </summary>
         /// <param name="factory">
-        /// First parameter: The source difference.<br/>
+        /// Null value is allowed here and means that a default behavior of creation of the difference is required.
+        /// Func first parameter: The args for the difference creation.<br/>
         /// Returns: Transformed difference or the source difference itself.
         /// </param>
         /// <exception cref="ArgumentNullException"></exception>
@@ -52,20 +53,6 @@ namespace ObjectsComparer
             DifferenceFactory = factory ?? throw new ArgumentNullException(nameof(factory));
 
             return this;
-        }
-    }
-    public class CreateDifferenceArgs 
-    {
-        public object RawValue1 { get; }
-
-        public object RawValue2 { get; }
-        public Difference DefaultDifference { get; }
-
-        public CreateDifferenceArgs(Difference defaultDifference, object rawValue1 = null, object rawValue2 = null)
-        {
-            DefaultDifference = defaultDifference ?? throw new ArgumentNullException(nameof(defaultDifference));
-            RawValue1 = rawValue1;
-            RawValue2 = rawValue2;
         }
     }
 }
